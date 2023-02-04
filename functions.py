18eb6b1893b11420ea6c28384e09f5db3bbc5e35
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-02-04 22:59:37 +0100

    Update

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 from flask import json
+import requests
+import psutil
+import socket
 
 def max_upload_size():
     data = json.load(open("./json/config.json"))["upload_size"]
@@ -9,3 +12,21 @@ def get_local_version():
     data = json.load(open("./json/runtime.json"))["version_local"]
     
     return data.rstrip()
+
+def get_online_version():
+    data = requests.get("https://raw.githubusercontent.com/Salware-Foundations/HomeNET/main/db/ver.txt")
+    
+    return data.text.rstrip()
+
+def memory_usage():
+    return int(psutil.virtual_memory().total - psutil.virtual_memory().available)
+
+
+def cpu_usage():
+    return psutil.cpu_percent()
+
+
+def get_ipaddr():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
--- main.py
+++ main.py
@@ -15,3 +15,53 @@ from werkzeug.utils import secure_filename
 from language import *
 from functions import *
 
+import time
+import os
+
+# Global Variables
+template_path                   = json.load(open("./json/runtime.json"))["template_path"]
+static_url_path                 = json.load(open("./json/runtime.json"))["static_url_path"]
+static_folder                   = json.load(open("./json/runtime.json"))["static_folder"]
+address                         = json.load(open("./json/runtime.json"))["address"]
+ip_type                         = json.load(open("./json/runtime.json"))["ip_type"]
+port                            = json.load(open("./json/runtime.json"))["port"]
+debug_mode                      = json.load(open("./json/runtime.json"))["debug"]
+
+
+upload_size                     = json.load(open("./json/config.json"))["upload_size"]
+
+github_release                  = "github.com"
+build_date                      = "2022-04-02"
+build_version                   = "2202.04"
+version                         = json.load(open("./json/runtime.json"))["version_local"]
+codename                        = "Vanilla Cake"
+edition_version                 = "Developer Preview"
+uptime                          = time.time()
+
+
+# App Declaration
+system = Flask(__name__, template_folder=template_path, static_url_path=static_url_path, static_folder=static_folder)
+
+system.config["MAX_CONTENT_LENGTH"] = int(upload_size) * 1024^2    # Max upload size in MegaBytes
+system.config["SECRET_KEY"] = "rnloJIYsiMVrbPxpDJBR2Em3YuUO6f"     # Secret key for session. Change this to something else.
+
+
+''' 
+    * -- HomeNET Routes -- *
+'''
+
+if ip_type == "auto":
+    ip = get_ipaddr()
+
+elif ip_type == "local":
+    ip = "localhost"
+
+elif ip_type == "all":
+    ip = "0.0.0.0"
+
+else:
+    ip = "localhost"
+
+# Start HomeNET
+if __name__ == "__main__":
+    system.run(host=ip, port=port, debug=debug_mode)
